606f31af92f752178237a8b5755b2d54cb2752ee
vuminhhung <vuminhhungltt904@gmail.com>
2023-09-29 08:30:52 +0700

    aaaa hieu an db

---
+++ src/components/layout/footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (`<footer> 
+      <div class=" bg-red-600 text-[20px] text-blue-600" >
+          this is page footer
+      </div>
+    </footer>`);
+}
---
+++ src/components/layout/header.js
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (`<header> 
+    <div class=" bg-orange-500 text-[20px] text-green-600" >
+        this is page header
+    </div>
+  </header>`);
+}
